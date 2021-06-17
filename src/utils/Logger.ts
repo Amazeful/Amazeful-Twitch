bf834c1c03186e87889567f347427117d9400f20
Aiden Hadisi <so.hadisi@gmail.com>
2021-06-17 02:07:46 -0700

    add winston logger

---
+++ src/utils/Logger.ts
@@ -0,0 +1,98 @@
+import {
+  createLogger,
+  format,
+  transports,
+  Logger as WinstonLogger,
+} from "winston";
+import chalk from "chalk";
+import { injectable, singleton } from "tsyringe";
+
+export enum LogLevel {
+  ERROR = "error",
+  WARN = "warn",
+  INFO = "info",
+  VERBOSE = "verbose",
+  DEBUG = "debug",
+  SILLY = "silly",
+}
+
+const LogLevelToEmoji: { [key: string]: string } = {
+  [LogLevel.ERROR]: "\u{274C}",
+  [LogLevel.WARN]: "\u{26A0}\u{FE0F} ",
+  [LogLevel.INFO]: "\u{2139}\u{FE0F} ",
+  [LogLevel.DEBUG]: "\u{1F41E}",
+  [LogLevel.SILLY]: "\u{1F43E}",
+};
+
+const consoleFormat = format.printf(({ level, message, timestamp, module }) => {
+  var upperLevel = ` ${level.toUpperCase()} `;
+  var emoji = LogLevelToEmoji[level];
+  switch (level) {
+    case LogLevel.SILLY:
+      message = chalk.magenta(message);
+      upperLevel = chalk.bgMagenta.bold(upperLevel);
+      break;
+
+    case LogLevel.DEBUG:
+      message = chalk.cyan(message);
+      upperLevel = chalk.bgCyan.bold(upperLevel);
+      break;
+
+    case LogLevel.VERBOSE:
+      message = chalk.magentaBright(message);
+      upperLevel = chalk.bgMagentaBright.bold(upperLevel);
+      break;
+
+    case LogLevel.INFO:
+      message = chalk.green(message);
+      upperLevel = chalk.bgGreen.bold(upperLevel);
+      break;
+
+    case LogLevel.WARN:
+      message = chalk.yellow(message);
+      upperLevel = chalk.black.bgYellowBright.bold(upperLevel);
+      break;
+
+    case LogLevel.ERROR:
+      message = chalk.red(message);
+      upperLevel = chalk.bgRedBright.bold(upperLevel);
+      break;
+
+    default:
+      break;
+  }
+
+  return `[${timestamp}] ${emoji} ${upperLevel} ${
+    module ? chalk.black.bgWhite(` ${module.toUpperCase()} `) + " " : ""
+  }${message}`;
+});
+
+const fileFormat = format.printf(({ level, message, timestamp, module }) => {
+  return `[${timestamp}] [${level.toUpperCase()}] ${
+    module ? "[" + module.toUpperCase() + "] " : ""
+  }${message}`;
+});
+
+export class Logger {
+  private static winstonLogger: WinstonLogger = createLogger({
+    level:
+      process.env.NODE_ENV === "development" ? LogLevel.SILLY : LogLevel.SILLY,
+    format: format.combine(format.timestamp(), format.splat(), consoleFormat),
+    transports: [
+      new transports.Console(),
+      new transports.File({
+        filename: `.logs/error.log`,
+        level: LogLevel.ERROR,
+        format: format.combine(format.timestamp(), format.splat(), fileFormat),
+      }),
+      new transports.File({
+        filename: `.logs/combined.log`,
+        format: format.combine(format.timestamp(), format.splat(), fileFormat),
+      }),
+    ],
+  });
+
+  public static log(level: LogLevel, message: string, module?: string) {
+    this.winstonLogger.log(level, message, { module: module });
+  }
+}
