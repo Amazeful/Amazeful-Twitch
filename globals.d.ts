1ff17b84fe614dfd6c1add282aa55254bb5dae63
Aiden Hadisi <so.hadisi@gmail.com>
2021-06-19 13:42:10 -0700

    Add declarations for global object

---
+++ globals.d.ts
@@ -0,0 +1,13 @@
+import { Bot } from "./lib/core/Bot";
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      NODE_ENV: "development" | "production";
+      BOTSTATUS: "default" | "knownBot" | "verifiedBot";
+      USERNAME: string;
+      PASSWORD: string;
+    }
+  }
+  var bot: Bot;
+}
