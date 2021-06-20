58d812471589cf48d48c863af3c5a85ba0cf4969
Aiden Hadisi <so.hadisi@gmail.com>
2021-06-19 23:34:12 -0700

    Add tests

---
+++ src/decorators/__tests__/DebugLogger.test.ts
@@ -0,0 +1,24 @@
+import { Logger, LogLevel } from "../../utils/Logger";
+import { DebugLogger } from "../DebugLogger";
+
+class TestClass {
+  @DebugLogger
+  public test() {
+    return "test";
+  }
+}
+describe("./decorators/DebugLogger", () => {
+  test("Should call the logger", () => {
+    const mockLog = jest.fn();
+    Logger.log = mockLog;
+
+    var testClass = new TestClass();
+
+    testClass.test();
+
+    expect(mockLog).toBeCalled();
+    expect(mockLog.mock.calls[0][0]).toBe(LogLevel.DEBUG);
+    expect(mockLog.mock.calls[0][1]).toBe("test called with: <No Args>");
+    expect(mockLog.mock.calls[0][2]).toBe("TestClass");
+  });
+});
---
+++ src/decorators/__tests__/TryCatchLog.test.ts
@@ -0,0 +1,31 @@
+import { Logger, LogLevel } from "../../utils/Logger";
+import { DebugLogger } from "../DebugLogger";
+import { TryCatchLog } from "../TryCatchLog";
+
+class TestClass {
+  @TryCatchLog
+  public test() {
+    throw new Error("failed");
+  }
+}
+describe("./decorators/TryCathLog", () => {
+  test("Should call the logger twice", () => {
+    const mockLog = jest.fn();
+    Logger.log = mockLog;
+
+    var testClass = new TestClass();
+
+    //should NOT throw
+    testClass.test();
+    expect(mockLog).toBeCalledTimes(2);
+    //Should debug log the function call data
+    expect(mockLog.mock.calls[0][0]).toBe(LogLevel.DEBUG);
+    expect(mockLog.mock.calls[0][1]).toBe("test called with: <No Args>");
+    expect(mockLog.mock.calls[0][2]).toBe("TestClass");
+
+    //Then it should catch and logs errors
+    expect(mockLog.mock.calls[1][0]).toBe(LogLevel.ERROR);
+    expect(mockLog.mock.calls[1][1]).toBe("test Error: failed");
+    expect(mockLog.mock.calls[1][2]).toBe("TestClass");
+  });
+});
