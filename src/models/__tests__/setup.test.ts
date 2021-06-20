64c21b5c57ec25caf9f7785373d693e97d3ae60d
Aiden Hadisi <so.hadisi@gmail.com>
2021-06-20 01:13:40 -0700

    fix db test setup script error

--- src/models/__tests__/setup.test.ts
+++ src/models/__tests__/setup.test.ts
@@ -5,14 +5,19 @@ var orm: MikroORM<IDatabaseDriver<Connection>>;
 var id = 0;
 export async function setupORM() {
   orm = await MikroORM.init<MongoDriver>({
-    entities: ["./lib/models/*.ts", "./lib/models/embeddables/*.ts"],
+    entities: ["./src/models/*.ts", "./src/models/embeddables/*.ts"],
     dbName: "test",
     type: "mongo",
     ensureIndexes: true,
     useBatchInserts: true,
     useBatchUpdates: true,
+    driverOptions: {
+      poolSize: 5,
+      ssl: false,
+    },
   });
   var driver = orm.em.getDriver() as MongoDriver;
+
   await driver.dropCollections();
   await driver.ensureIndexes();
 }
