9d6f8fe7e74fdb4d1aa4a1e58a483bfe5ee244a3
Aiden Hadisi <so.hadisi@gmail.com>
2021-06-24 17:38:16 -0700

    create main script

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,17 @@
+import "reflect-metadata";
+
 import { Bot } from "./core/Bot";
 import { EnvSchema } from "./validators/schemas/EnvSchema";
 import * as dotenv from "dotenv";
 import { Logger, LogLevel } from "./utils/Logger";
-import { TwitchData } from "./types/TwitchAuth";
-
 (async () => {
-  var result = EnvSchema.validate(dotenv.config());
+  var result = EnvSchema.validate(dotenv.config().parsed);
   if (result.error) {
     Logger.log(LogLevel.ERROR, result.error.message);
     process.exit();
   } else {
     Object.assign(process.env, result.value);
-    let twitchData: TwitchData = {
-      username: process.env.USERNAME,
-      password: process.env.PASSWORD,
-      botStatus: process.env.BOTSTATUS,
-      clientID: "",
-      clientSecret: "",
-      accessToken: "",
-      refreshToken: "",
-    };
-    globalThis.bot = new Bot(twitchData);
+    var bot = new Bot();
     await bot.init();
   }
 })();
