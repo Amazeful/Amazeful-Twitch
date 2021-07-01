e5bd22bf7485da7998f6a528d417de9d6ffa8e29
Aiden Hadisi <so.hadisi@gmail.com>
2021-07-01 04:41:41 -0700

    adds cachemanager service

--- src/services/CahceManager.ts
+++ src/services/CahceManager.ts
@@ -1,11 +1,24 @@
-import Redis from "ioredis";
+import Redis, { KeyType } from "ioredis";
 import { singleton } from "tsyringe";
+import { CacheSetConfig } from "../types/CacheSetConfig";
 
 @singleton()
 export class CacheManager extends Redis {
   private _totalCalled: number = 0;
   private _totalHit: number = 0;
   private _totalMiss: number = 0;
+
+  public cache(data: CacheSetConfig): Promise<"OK" | null> {
+    return super.set(data.key, data.value, "PX", data.expiry);
+  }
+
+  public async get(key: KeyType): Promise<string | null> {
+    this._totalCalled++;
+    var result = await super.get(key);
+    if (!result) this._totalMiss++;
+    else this._totalHit++;
+    return result;
+  }
 
   public get totalCalled(): number {
     return this._totalCalled;
