a60d4d84d36dcec6209d087880a9305dd58aedf4
AidenHadisi <so.hadisi@gmail.com>
2021-07-10 14:47:19 -0700

    add command handler types

--- src/types/Misc.ts
+++ src/types/Misc.ts
@@ -1,15 +1,29 @@
 import { PrivmsgMessage } from "dank-twitch-irc";
-import { CacheManager } from "../services/CahceManager";
+import { Module } from "../modules/Module";
+
+//Defines an object constructor type
 export type Constructor<T> = {
   new (...args: any[]): T;
 };
 
-export type MessageHandler = {
-  (msg: PrivmsgMessage): void;
-};
-
+//Defines a type for Cacheable method decorators.
+//Cacheable methods must return a Promise
 export type CacheableMethodDecorator = <T>(
   target: Object,
   propertyKey: string,
   descriptor: TypedPropertyDescriptor<(...args: any[]) => Promise<T>>
+) => void;
+
+// CommandHandlerType defines a type of command handler method within modules
+export type CommandHandlerType = (
+  sender: string,
+  args: string[]
+) => Promise<string>;
+
+// CommandHandlerDecorator defines a type for command handler decorators.
+// Command handler methods must return a Promise
+export type CommandHandlerDecorator = (
+  target: Module,
+  propertyKey: string,
+  descriptor: TypedPropertyDescriptor<CommandHandlerType>
 ) => void;
