130a6eb3f43a9aaccddc07c4f7d517c009d73298
Aiden Hadisi <so.hadisi@gmail.com>
2021-07-23 22:56:55 -0700

    Add TwitchData

---
+++ src/types/TwitchData.ts
@@ -0,0 +1,7 @@
+//TwitchData includes required twitch information
+export interface TwitchData {
+  username: string;
+  password: string;
+  clientID: string;
+  clientSecret: string;
+}
--- src/validators/schemas/EnvSchema.ts
+++ src/validators/schemas/EnvSchema.ts
@@ -2,7 +2,7 @@ import Joi from "joi";
 
 export const EnvSchema = Joi.object({
   NODE_ENV: Joi.string()
-    .default("production")
+    .default("development")
     .valid("production", "development"),
   BOTSTATUS: Joi.string()
     .default("default")
@@ -11,7 +11,9 @@ export const EnvSchema = Joi.object({
   PASSWORD: Joi.string().required(),
   CLIENT_ID: Joi.string().required(),
   CLIENT_SECRET: Joi.string().required(),
-  SHARD_ID: Joi.number().integer(),
+  ACCESS_TOKEN: Joi.string().required(),
+  REFRESH_TOKEN: Joi.string().required(),
+  SHARD_ID: Joi.number().integer().default(0),
 })
   .required()
-  .label("env file");
+  .label(".env file");
