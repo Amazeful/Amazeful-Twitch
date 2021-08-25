7d89475dc0c2e84b3220e89b3db76a54c4f6d572
Aiden Hadisi <so.hadisi@gmail.com>
2021-08-25 14:17:09 -0700

    Add purge options validation schema

---
+++ src/validators/PurgeSchema.ts
@@ -0,0 +1,28 @@
+import Joi from "joi";
+
+export const PurgeSchema = Joi.object({
+  lookbackTime: Joi.number()
+    .integer()
+    .min(10)
+    .max(600)
+    .required()
+    .label("lookback time"),
+  timeoutDuration: Joi.alternatives()
+    .try(
+      Joi.number().integer().integer().min(1).max(1209600).required(),
+      Joi.string().lowercase().valid("ban")
+    )
+    .label("timeout duration"),
+  phrase: Joi.string().min(3).required(),
+  regex: Joi.boolean().required(),
+  modName: Joi.string().required(),
+  continuous: Joi.boolean().required(),
+  continuousTime: Joi.number()
+    .integer()
+    .min(10)
+    .max(600)
+    .required()
+    .label("continuous time"),
+})
+  .required()
+  .label("Purge Options");
